6d7e343392d8257189be3de49ed855db570e03f9
徐秋影 <1373616834@qq.com>
2019-03-14 16:09:01 +0800

    add library setting

---
+++ src/SoftLibrary/setting.py
@@ -0,0 +1,11 @@
+APPNAME = 'putty.exe'
+USERNAME = 'root'
+PASSWORD = '1997zxcV'
+COMMAND = 'll'
+IP = '120.78.127.166'
+
+
+CLOSE_STATES = ['Always', 'Never', 'Only on clean exit']
+
+PAUSE = 1.5
+
