4017d8dc235bdb25be7af5dd19680998f635e7b6
徐秋影 <1373616834@qq.com>
2019-03-14 16:05:32 +0800

    自动化测试数据

---
+++ resources/test_data.py
@@ -0,0 +1,14 @@
+IP = '120.78.127.166'
+DICT__CURRENT_DATA = {
+    'user': 'root',
+            'password': '1997zxcV',
+            'command': 'll'
+}
+
+DICT__ERROR_DATA = {
+    'user': 'root',
+            'error': '197zxcV',
+            'password': '1997zxcV',
+            'command': 'll'
+}
+
